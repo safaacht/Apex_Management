8fa72e7d6d5bae20f43f98d9f69bcf6340a4948c
safaacht <safaachtaoui@gmail.com>
2025-12-30 23:20:48 +0100

    creation d'une classe abstret et son construct

---
+++ Personne.php
@@ -0,0 +1,13 @@
+<?php
+
+abstract class Personne{
+    
+
+    public function __construct(protected string $name,protected string $nationalite,protected string $email,protected Contract $contract,protected ?int $id=null)
+    {
+    }
+
+    abstract public function getAnnualCost();
+//     - Le Joueur coûte : (Salaire mensuel * 12) + Prime de signature.
+// - Le Coach coûte : (Salaire mensuel * 12) + Frais de déplacement.
+}
