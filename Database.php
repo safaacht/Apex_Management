bd6ac535d0acc2bcdc2c9575477629c0bd3e4e10
safaacht <safaachtaoui@gmail.com>
2025-12-30 18:22:59 +0100

    creation d'une connection database static

---
+++ Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database{
+    private static $host="localhost";
+    private static $db_name="apex_management";
+    private static $user="root";
+    private static $mdp="";
+    private static $port=3307;
+
+    private static $conn=null;
+
+    public static function getConnection(){
+        if(self::$conn===null){
+            $dsn="mysql:host=" . self::$host . ";dbname=" . self::$db_name . ";port=" . self::$port . ";charset=utf8";
+            self::$conn= new PDO($dsn,self::$user,self::$mdp,[
+                PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION
+            ]);
+        }
+        return self::$conn;
+    }
+   
+} 
+$db = Database::getConnection();
+if($db){
+    echo "Connexion réussie";
+}
+
