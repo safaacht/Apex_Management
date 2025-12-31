76b69639b6f14a92505f294ed99992b18db4b1e3
safaacht <safaachtaoui@gmail.com>
2025-12-31 17:23:17 +0100

    visiblité des bouttons ca depend du loging

--- ApexMercato/header.php
+++ ApexMercato/header.php
@@ -1,11 +1,15 @@
 <header>
     <div class="logo">APEX<span>Management</span></div>
     <nav>
+        <a href="../ApexMercato/index.php">Home</a>
+        <a href="../views/dashboard.php">Dashboard</a>
+        <?php if(isset($_SESSION['user_id'])){?>
+            <a href="../views/JoueurForm.php">Players</a>
+            <a href="../views/CoachForm.php">Coaches</a>
+            <a href="../actions/logout.php">Logout</a>
+        <?php } ?>   
         <a href="../views/LoginForm.php">Login</a>
         <a href="../views/SignupForm.php">Sign Up</a>
-        <a href="../views/JoueurForm.php">Players</a>
-        <a href="../views/CoachForm.php">Coaches</a>
-        <a href="../ApexMercato/index.php">Home</a>
     </nav>
 </header>
 
@@ -41,4 +45,3 @@ header nav a:hover{
     color:#facc15;
 }
 </style>
-
