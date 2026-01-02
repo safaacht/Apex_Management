1eb1f1cf9cd939e2ea576973101ced3a497b79ab
safaacht <safaachtaoui@gmail.com>
2026-01-02 01:18:21 +0100

    modification des liens

--- views/home.php
+++ views/home.php
@@ -62,8 +62,8 @@ button:hover{
     <h1>Welcome to <span>APEX MERCATO</span></h1>
     <p>Your e-Sport management platform. Manage players, coaches, and tournaments easily.</p>
     <div>
-        <button onclick="location.href='signup.php'">Sign Up</button>
-        <button onclick="location.href='login.php'">Login</button>
+        <button onclick="location.href='../views/SignupForm.php'">Sign Up</button>
+        <button onclick="location.href='../views/LoginForm.php'">Login</button>
     </div>
 </main>
 
