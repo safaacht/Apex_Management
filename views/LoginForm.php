1826b001c119ea33e663d49d9013a67a41e69517
safaacht <safaachtaoui@gmail.com>
2026-01-02 01:03:49 +0100

    modification d'input submit

--- views/LoginForm.php
+++ views/LoginForm.php
@@ -12,8 +12,8 @@ h2{text-align:center;margin-bottom:25px;color:#22d3ee;letter-spacing:1px;}
 label{font-size:14px;color:#9ca3af;margin-bottom:6px;display:block;}
 input{width:100%;padding:12px;margin-bottom:18px;background:#020617;border:1px solid #1f2937;border-radius:8px;color:#e5e7eb;}
 input:focus{outline:none;border-color:#7c3aed;box-shadow:0 0 8px rgba(124,58,237,.6);}
-button{width:100%;padding:12px;background:linear-gradient(135deg,#7c3aed,#22d3ee);border:none;border-radius:10px;color:#020617;font-size:16px;font-weight:bold;cursor:pointer;transition:.3s;}
-button:hover{transform:translateY(-2px);box-shadow:0 0 18px rgba(34,211,238,.6);}
+#button{width:100%;padding:12px;background:linear-gradient(135deg,#7c3aed,#22d3ee);border:none;border-radius:10px;color:#020617;font-size:16px;font-weight:bold;cursor:pointer;transition:.3s;}
+#button:hover{transform:translateY(-2px);box-shadow:0 0 18px rgba(34,211,238,.6);}
 .link{text-align:center;margin-top:15px;font-size:14px;}
 .link a{color:#facc15;text-decoration:none;font-weight:bold;}
 </style>
@@ -26,12 +26,12 @@ button:hover{transform:translateY(-2px);box-shadow:0 0 18px rgba(34,211,238,.6);
 <main>
     <div class="form-box">
         <h2>LOGIN</h2>
-        <form>
+        <form method="post">
             <label>Email</label>
             <input type="email" name="email" required>
             <label>Password</label>
             <input type="password" name="password" required>
-            <button>LOGIN</button>
+            <input id="button" type="submit" name="login" value="LOGIN"> 
         </form>
         <div class="link">
             No account? <a href="../views/SignupForm.php">Create one</a>
