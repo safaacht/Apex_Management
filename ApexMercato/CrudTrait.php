822bc329f281c2f6188ec0ca35ac0c43b4f5c50c
safaacht <safaachtaoui@gmail.com>
2026-01-05 10:25:02 +0100

    dynamic crud

---
+++ ApexMercato/CrudTrait.php
@@ -0,0 +1,62 @@
+<?php
+namespace Apex\Core;
+use Apex\Database\Database;
+use PDO;
+
+trait CrudTrait{
+ protected PDO $conn;
+ protected string $table;
+ 
+//  construct
+protected function initCrud():void
+{
+    $this->conn=Database::getConnection();
+}
+
+// creat
+public function creat(array $data):bool
+{
+    $columns=implode(', ',array_keys($data));
+    $placeholders=":".implode(", :", array_keys($data));
+
+    $sql="INSERT INTO {$this->table} ($columns) VALUES ($placeholders)";
+    $stmt=$this->conn->prepare($sql);
+    $rslt=$stmt->execute($data);
+
+    return $rslt;
+}
+
+// read
+public function all():array{
+    $sql="SELECT * FROM {$this->table}";
+    $stmt=$this->conn->query($sql);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// delete
+public function delete(int $id):bool
+{
+    $stmt=$this->conn->prepare("
+    SELECT * FROM {$this->table} WHERE id= :id
+    ");
+
+    return $stmt->execute(["id"=>$id]);
+}
+
+// update
+public function update(int $id, array $data):bool
+{
+    $fields="";
+    foreach($data as $key=>$value){
+        
+        $fields .= "$key :$key,";
+    }
+    $fields=rtrim($fields,",");
+    $data['id']=$id;
+    
+    $stmt=$this->conn->prepare(
+        "UPDATE {$this->table} SET $fields WHERE id= :id"
+    );
+    return $stmt->execute($data);
+}
+}
