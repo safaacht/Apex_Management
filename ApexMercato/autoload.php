f9de58fc62c0d2b66148d80aa5c26b0c9ca07dc4
safaacht <safaachtaoui@gmail.com>
2026-01-05 11:00:54 +0100

    dynamic autoloading

---
+++ ApexMercato/autoload.php
@@ -0,0 +1,11 @@
+<?php
+spl_autoload_register(function ($class) {
+
+    $baseDir = dirname(__DIR__) . '/';
+
+    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
