d786d1bb34e0aaa9cf745bb1ef6fd365f7e9890d
safaacht <safaachtaoui@gmail.com>
2026-01-05 11:30:01 +0100

    fixer le path d'autoloading

--- ApexMercato/autoload.php
+++ ApexMercato/autoload.php
@@ -1,9 +1,7 @@
 <?php
 spl_autoload_register(function ($class) {
 
-    $baseDir = dirname(__DIR__) . '/';
-
-    $file = $baseDir . str_replace('\\', '/', $class) . '.php';
+    $file = __DIR__ . '/../'. str_replace('\\', '/', $class) . '.php';
 
     if (file_exists($file)) {
         require_once $file;
