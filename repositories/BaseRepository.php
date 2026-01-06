aaf19f644ea3903af731a5e7659c76fa9fcf960b
safaacht <safaachtaoui@gmail.com>
2026-01-06 11:34:48 +0100

    repository pour dynamique crud

---
+++ repositories/BaseRepository.php
@@ -0,0 +1,74 @@
+ <?php
+use ApexMercato\Database;
+use PDO;
+
+class BaseRepository{
+
+    protected PDO $conn;
+    protected string $table;
+    
+    //  construct
+    protected function initCrud():void
+    {
+        $this->conn=Database::getConnection();
+    }
+    
+    // creat
+    public function create(array $data):bool
+    {
+        $columns=implode(', ',array_keys($data));
+        $placeholders=":".implode(", :", array_keys($data));
+        
+        $sql="INSERT INTO {$this->table} ($columns) VALUES ($placeholders)";
+        $stmt=$this->conn->prepare($sql);
+        $rslt=$stmt->execute($data);
+        
+        return $rslt;
+    }
+    
+    // read
+    public function all():array
+    {
+        $sql="SELECT * FROM {$this->table}";
+        $stmt=$this->conn->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+// delete
+public function delete(int $id):bool
+{
+    $stmt=$this->conn->prepare("
+    DELETE FROM {$this->table} WHERE id= :id
+    ");
+    
+    return $stmt->execute(["id"=>$id]);
+}
+
+// update
+public function update(int $id, array $data):bool
+{
+    $fields="";
+    foreach($data as $key=>$value){
+        
+        $fields .= "$key=:$key,";
+    }
+    $fields=rtrim($fields,",");
+    $data['id']=$id;
+    
+    $stmt=$this->conn->prepare(
+        "UPDATE {$this->table} SET $fields WHERE id= :id"
+    );
+    return $stmt->execute($data);
+}
+
+// affichage
+public function save(array $data){
+    $keys = array_keys($data);
+    $values = array_values($data);
+
+    $sql = "INSERT INTO " . $this->table . "( " . implode(', ', $keys) . " ) VALUES ( :" .  implode(', : ', $values) . ")";
+    var_dump($sql);
+}
+
+}
+
