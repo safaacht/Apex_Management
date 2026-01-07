d008ca7d702789bae59e231dd40c70b67f97f1ba
safaacht <safaachtaoui@gmail.com>
2026-01-07 15:28:40 +0100

    initialisation du fichier dashbord d'admin seulement

---
+++ views/dashboardAdmin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>APEX MERCATO - Home</title>
+</head>
+<body>
+    <?php require_once '../ApexMercato/header.php' ?>
+
+
+<?php require_once '../ApexMercato/footer.php' ?>
+
+
+</body>
+</html>
